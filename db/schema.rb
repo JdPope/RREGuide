00a9bb1a4b18041d12e395148b65d8c37586df35
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-25 10:20:59 -0600

    Working through auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_155427) do
+ActiveRecord::Schema.define(version: 2019_07_22_215625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 2019_07_22_155427) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["expectation_id"], name: "index_tasks_on_expectation_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "bio"
+    t.string "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "expectations", "responsibilities"

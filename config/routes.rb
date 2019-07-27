cf6489178dc74e193001cea0bd9b82f10e84a410
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-27 08:49:04 -0600

    Revert "Working through auth"
    This reverts commit 00a9bb1a4b18041d12e395148b65d8c37586df35.

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,5 @@ Rails.application.routes.draw do
   resources :roles
   resources :jobs
   resources :admins
-  resources :users, only: [:create]
-      post '/login', to: 'auth#create'
-      get '/profile', to: 'users#profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
